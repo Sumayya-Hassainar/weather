054ec6fa3927d17cc42b524dc49fb7c8ae1e97f9
sumayya <sumayyahassainar37@gmail.com>
2025-10-31 21:37:07 +0530

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: './',  // ✅ ensures correct relative imports for JS/CSS when hosted
   build: {
-    // Optional: increase or silence the large chunk warning
-    chunkSizeWarningLimit: 1000, // or 2000 if your app is large
-    sourcemap: false, // optional: omit source maps for smaller builds
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false,
   },
 })
