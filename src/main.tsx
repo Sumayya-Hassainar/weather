910f27c87ade6b866335192fd12b66721dc64cfc
sumayya <sumayyahassainar37@gmail.com>
2025-10-31 21:30:29 +0530

    update css

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import Home from './pages/Home'
-
+import './App.css'
 const App = () => (
   <Provider store={store}>
     <div style={{ fontFamily: 'Inter, system-ui, sans-serif', background: '#f7f7f8', minHeight: '100vh', padding: 20 }}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   build: {
     // Optional: increase or silence the large chunk warning
     chunkSizeWarningLimit: 1000, // or 2000 if your app is large
